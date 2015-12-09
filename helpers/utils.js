e25b21965e96ccd8f5f6383ad7b1a30433786923
Gordey Levchenko <g.levchenko@rambler-co.ru>
2015-12-09 13:49:32 +0300

    Load sprite after document loaded

--- helpers/utils.js
+++ helpers/utils.js
@@ -262,7 +262,7 @@ module.exports.svgXHR = function(filename, baseUrl) {
 
   baseUrl = (typeof baseUrl !== 'undefined') ? ', \''+ baseUrl +'\'': '';
 
-  wrapper += 'window.onload = function() { svgXHR(\'' + filename + '\''+ baseUrl +'); }';
+  wrapper += 'document.addEventListener(\'DOMContentLoaded\', svgXHR(\'' + filename + '\''+ baseUrl +'), false);';
   return wrapper;
 };
 
