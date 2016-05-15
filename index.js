6973767c3e43834216f9c2b20d29e1c6e069ddea
Aaron Franks <aaron.franks@gmail.com>
2016-05-15 13:27:56 -0700

    Use new ConcatSource dependency for webpack 2.x

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ var path = require('path');
 var url = require('url');
 var slash = require('slash');
 var utils = require('./helpers/utils');
-var ConcatSource = require('webpack/lib/ConcatSource');
 var ModuleFilenameHelpers = require('webpack/lib/ModuleFilenameHelpers');
+
+var ConcatSource;
+try {
+  ConcatSource = require('webpack/lib/ConcatSource');       // webpack 1.x
+} catch (e) {
+  ConcatSource = require('webpack-sources').ConcatSource;   // webpack 2.x
+}
 
 /**
  * Constructor
