bdc7e4c7f1e449e10702d4f0cdbd368708f1844c
Chernobrov Mike <m.chernobrov@rambler-co.ru>
2016-08-23 19:50:22 +0300

    test promises

--- src/svgstore.js
+++ src/svgstore.js
@@ -36,106 +36,184 @@ WebpackSvgStore.prototype.apply = function(compiler) {
   var tasks = [];
   var options = this.options;
 
-  /**
-   * @param  {[type]}   unused [description]
-   * @param  {Function} done   [description]
-   * @return {[type]}          [description]
-   */
-  function run(unused, done) {
-    function analyzeAst(resolve) {
-      /**
-       * Filing properties
-       * @param  {[type]} expr [description]
-       * @param  {[type]} data [description]
-       * @return {[type]}      [description]
-       */
-      var fillProps = function(expr, props) {
-        expr.init.properties.forEach(function(prop) {
-          switch (prop.key.name) {
-            case 'name': props.fileName = prop.value.value; break;
-            case 'path': props.path = prop.value.value; break;
-            default: break;
-          }
-        });
+  // /**
+  //  * @param  {[type]}   unused [description]
+  //  * @param  {Function} done   [description]
+  //  * @return {[type]}          [description]
+  //  */
+  // function run(unused, done) {
+  //   function analyzeAst(resolve) {
+  //     /**
+  //      * Filing properties
+  //      * @param  {[type]} expr [description]
+  //      * @param  {[type]} data [description]
+  //      * @return {[type]}      [description]
+  //      */
+  //     var fillProps = function(expr, props) {
+  //       expr.init.properties.forEach(function(prop) {
+  //         switch (prop.key.name) {
+  //           case 'name': props.fileName = prop.value.value; break;
+  //           case 'path': props.path = prop.value.value; break;
+  //           default: break;
+  //         }
+  //       });
+  //
+  //       props.state = this.state;
+  //       props.context = this.state.current.context;
+  //
+  //       return props;
+  //     };
+  //
+  //     /**
+  //      * Filing tasks
+  //      * @param  {[type]} expr [description]
+  //      * @param  {[type]} data [description]
+  //      * @return {[type]}      [description]
+  //      */
+  //     var fillTasks = function(expr, props) {
+  //       var spriteFolder = path.join(props.context, props.path || '');
+  //       utils.filesMap(spriteFolder, function(files) {
+  //         var dep;
+  //         var replacement;
+  //         var fileName = props.fileName;
+  //         var fileContent = utils.createSprite(
+  //           utils.parseFiles(files, options),
+  //           options.template
+  //         );
+  //
+  //         // check files in current folder
+  //         files.length <= 0
+  //           ? props.state.compilation.errors.push('No files in: ' + spriteFolder + ' folder')
+  //           : null;
+  //
+  //         // if filename has [hash]
+  //         fileName.indexOf('[hash]') >= 0
+  //           ? fileName = utils.hash(props.fileName, CRC32.bstr(fileContent))
+  //           : null;
+  //
+  //         replacement = expr.id.name + ' = { filename: "' + fileName + '" }';
+  //         dep = new ConstDependency(replacement, expr.range);
+  //         // put expression location
+  //         dep.loc = expr.loc;
+  //         // add task for replacement
+  //         props.state.current.addDependency(dep);
+  //         // push task for emit event
+  //         tasks.push({ content: fileContent, name: fileName });
+  //       });
+  //     };
+  //
+  //     resolve({});
+  //
+  //     return function(expr) {
+  //       var props = {
+  //         path: '/**/*.svg',
+  //         fileName: '[hash].sprite.svg'
+  //       };
+  //       // fill data
+  //       props = fillProps.bind(this)(expr, props);
+  //
+  //       // fill tasks
+  //       fillTasks.bind(this)(expr, props);
+  //
+  //       return true;
+  //     };
+  //   }
+  //
+  //
+  //   compiler.parser.plugin('var __svg__', analyzeAst(resolve));
+  //   compiler.parser.plugin('var __sprite__', analyzeAst(resolve));
+  //   compiler.parser.plugin('var __svgstore__', analyzeAst(resolve));
+  //   compiler.parser.plugin('var __svgsprite__', analyzeAst(resolve));
+  //   compiler.parser.plugin('var __webpack_svgstore__', analyzeAst(resolve));
+  //
+  //
+  //   /**
+  //    * Wrap in promise for long timing actions
+  //    */
+  //   new Promise(function(resolve) {
+  //
+  //   })
+  //   .then(function(data) {
+  //     console.log(data);
+  //     // done();
+  //   })
+  //   .then(function() {
+  //     done();
+  //   });
+  // }
+  //
+  // compiler.plugin('run', run);
+  // compiler.plugin('watch-run', run);
 
-        props.state = this.state;
-        props.context = this.state.current.context;
-
-        return props;
+  var analyzeAst = function(expr) {
+    var _self = this;
+    var run = function(resolve, expr) {
+      var spriteFolder = '';
+      var props = {
+        path: '/**/*.svg',
+        fileName: '[hash].sprite.svg',
+        state: this.state
       };
 
-      /**
-       * Filing tasks
-       * @param  {[type]} expr [description]
-       * @param  {[type]} data [description]
-       * @return {[type]}      [description]
-       */
-      var fillTasks = function(expr, props) {
-        var spriteFolder = path.join(props.context, props.path || '');
-        utils.filesMap(spriteFolder, function(files) {
-          var dep;
-          var replacement;
-          var fileName = props.fileName;
-          var fileContent = utils.createSprite(
-            utils.parseFiles(files, options),
-            options.template
-          );
+      expr.init.properties.forEach(function(prop) {
+        switch (prop.key.name) {
+          case 'name': props.fileName = prop.value.value; break;
+          case 'path': props.path = prop.value.value; break;
+          default: break;
+        }
+      });
 
-          // check files in current folder
-          files.length <= 0
-            ? props.state.compilation.errors.push('No files in: ' + spriteFolder + ' folder')
-            : null;
+      spriteFolder = path.join(this.state.current.context, props.path || '');
 
-          // if filename has [hash]
-          fileName.indexOf('[hash]') >= 0
-            ? fileName = utils.hash(props.fileName, CRC32.bstr(fileContent))
-            : null;
+      utils.filesMap(spriteFolder, function(files) {
+        var dep;
+        var replacement;
+        var fileName = props.fileName;
+        var fileContent = utils.createSprite(
+          utils.parseFiles(files, options),
+          options.template
+        );
 
-          replacement = expr.id.name + ' = { filename: "' + fileName + '" }';
-          dep = new ConstDependency(replacement, expr.range);
-          // put expression location
-          dep.loc = expr.loc;
-          // add task for replacement
-          props.state.current.addDependency(dep);
-          // push task for emit event
-          tasks.push({ content: fileContent, name: fileName });
-        });
+        // check files in current folder
+        files.length <= 0
+          ? props.state.compilation.errors.push('No files in: ' + spriteFolder + ' folder')
+          : null;
+
+        // if filename has [hash]
+        fileName.indexOf('[hash]') >= 0
+          ? fileName = utils.hash(props.fileName, CRC32.bstr(fileContent))
+          : null;
+
+        replacement = expr.id.name + ' = { filename: "' + fileName + '" }';
+        dep = new ConstDependency(replacement, expr.range);
+
+        // put expression location
+        dep.loc = expr.loc;
+        // add task for replacement
+        props.state.current.addDependency(dep);
+        // push task for emit event
+        resolve({ content: fileContent, name: fileName });
+      });
+
+      return function(unused, done) {
+        done();
       };
+    };
 
-      resolve();
-
-      return function(expr) {
-        var props = {
-          path: '/**/*.svg',
-          fileName: '[hash].sprite.svg'
-        };
-        // fill data
-        props = fillProps.bind(this)(expr, props);
-
-        // fill tasks
-        fillTasks.bind(this)(expr, props);
-
-        return true;
-      };
-    }
-
-    /**
-     * Wrap in promise for long timing actions
-     */
     new Promise(function(resolve) {
-      compiler.parser.plugin('var __svg__', analyzeAst(resolve));
-      compiler.parser.plugin('var __sprite__', analyzeAst(resolve));
-      compiler.parser.plugin('var __svgstore__', analyzeAst(resolve));
-      compiler.parser.plugin('var __svgsprite__', analyzeAst(resolve));
-      compiler.parser.plugin('var __webpack_svgstore__', analyzeAst(resolve));
+      compiler.plugin('run', run.bind(_self)(resolve, expr));
+      // compiler.plugin('watch-run', run.bind(_self)(resolve, expr));
     })
-    .then(function() {
-      done();
+    .then(function(data) {
+      tasks.push(data);
     });
-  }
+  };
 
-  compiler.plugin('run', run);
-  compiler.plugin('watch-run', run);
+  compiler.parser.plugin('var __svg__', analyzeAst);
+  compiler.parser.plugin('var __sprite__', analyzeAst);
+  compiler.parser.plugin('var __svgstore__', analyzeAst);
+  compiler.parser.plugin('var __svgsprite__', analyzeAst);
+  compiler.parser.plugin('var __webpack_svgstore__', analyzeAst);
 
   compiler.plugin('emit', function(compilation, done) {
     new Promise(function(resolve) {
